83111030215a07af23bf52a11a225ebfe4d26d20
Rui Tang <talltangrui@outlook.com>
2019-04-09 11:44:34 +0800

    reduce vendor

---
+++ vendor/github.com/csi-driver/blobfuse-csi-driver/pkg/blobfuse/controllerserver.go
@@ -0,0 +1,233 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package blobfuse
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"k8s.io/kubernetes/pkg/volume/util"
+
+	azstorage "github.com/Azure/azure-sdk-for-go/storage"
+	"github.com/container-storage-interface/spec/lib/go/csi"
+	"github.com/pborman/uuid"
+	"k8s.io/klog"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+// const blobfuseAccountNamePrefix = "fuse"
+
+// CreateVolume provisions an blobfuse
+func (d *Driver) CreateVolume(ctx context.Context, req *csi.CreateVolumeRequest) (*csi.CreateVolumeResponse, error) {
+	if err := d.ValidateControllerServiceRequest(csi.ControllerServiceCapability_RPC_CREATE_DELETE_VOLUME); err != nil {
+		klog.Errorf("invalid create volume req: %v", req)
+		return nil, err
+	}
+
+	volumeCapabilities := req.GetVolumeCapabilities()
+	name := req.GetName()
+	if len(name) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "CreateVolume Name must be provided")
+	}
+	if len(volumeCapabilities) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "CreateVolume Volume capabilities must be provided")
+	}
+
+	volSizeBytes := int64(req.GetCapacityRange().GetRequiredBytes())
+	requestGiB := int(util.RoundUpSize(volSizeBytes, 1024*1024*1024))
+
+	parameters := req.GetParameters()
+	var storageAccountType, resourceGroup, location, accountName, containerName string
+
+	// Apply ProvisionerParameters (case-insensitive). We leave validation of
+	// the values to the cloud provider.
+	for k, v := range parameters {
+		switch strings.ToLower(k) {
+		case "skuname":
+			storageAccountType = v
+		case "storageaccounttype":
+			storageAccountType = v
+		case "location":
+			location = v
+		case "storageaccount":
+			accountName = v
+		case "resourcegroup":
+			resourceGroup = v
+		case "containername":
+			containerName = v
+		default:
+			return nil, fmt.Errorf("invalid option %q", k)
+		}
+	}
+
+	/* to-do
+	account, key, err := d.cloud.EnsureStorageAccount(accountName, storageAccountType, string(storage.StorageV2), resourceGroup, location, blobfuseAccountNamePrefix)
+	if err != nil {
+		return nil, fmt.Errorf("could not get storage key for storage account %s: %v", accountName, err)
+	}
+	*/
+
+	// now we only use existing storage account, will switch to create storage account with EnsureStorageAccount interface
+	accountKey, err := GetStorageAccesskey(d.cloud, accountName, resourceGroup)
+	if err != nil {
+		return nil, fmt.Errorf("no key for storage account(%s) under resource group(%s), err %v", accountName, resourceGroup, err)
+	}
+
+	if containerName == "" {
+		// now we set as 63 for maximum container name length
+		// todo: get cluster name
+		containerName = util.GenerateVolumeName("pvc-fuse", uuid.NewUUID().String(), 63)
+	}
+
+	klog.V(2).Infof("begin to create container(%s) on account(%s) type(%s) rg(%s) location(%s) size(%d)", containerName, accountName, storageAccountType, resourceGroup, location, requestGiB)
+	client, err := azstorage.NewBasicClientOnSovereignCloud(accountName, accountKey, d.cloud.Environment)
+	if err != nil {
+		return nil, err
+	}
+	blobClient := client.GetBlobService()
+	container := blobClient.GetContainerReference(containerName)
+	_, err = container.CreateIfNotExists(&azstorage.CreateContainerOptions{Access: azstorage.ContainerAccessTypePrivate})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create container(%s) on account(%s) type(%s) rg(%s) location(%s) size(%d), error: %v", containerName, accountName, storageAccountType, resourceGroup, location, requestGiB, err)
+	}
+
+	volumeID := fmt.Sprintf(volumeIDTemplate, resourceGroup, accountName, containerName)
+
+	/* todo: snapshot support
+	if req.GetVolumeContentSource() != nil {
+		contentSource := req.GetVolumeContentSource()
+		if contentSource.GetSnapshot() != nil {
+		}
+	}
+	*/
+	klog.V(2).Infof("create container %s on storage account %s successfully", containerName, accountName)
+
+	return &csi.CreateVolumeResponse{
+		Volume: &csi.Volume{
+			VolumeId:      volumeID,
+			CapacityBytes: req.GetCapacityRange().GetRequiredBytes(),
+			VolumeContext: parameters,
+		},
+	}, nil
+}
+
+// DeleteVolume delete an blobfuse
+func (d *Driver) DeleteVolume(ctx context.Context, req *csi.DeleteVolumeRequest) (*csi.DeleteVolumeResponse, error) {
+	if len(req.GetVolumeId()) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Volume ID missing in request")
+	}
+
+	if err := d.ValidateControllerServiceRequest(csi.ControllerServiceCapability_RPC_CREATE_DELETE_VOLUME); err != nil {
+		return nil, fmt.Errorf("invalid delete volume req: %v", req)
+	}
+
+	volumeID := req.VolumeId
+	resourceGroupName, accountName, containerName, err := getContainerInfo(volumeID)
+	if err != nil {
+		return nil, err
+	}
+
+	if resourceGroupName == "" {
+		resourceGroupName = d.cloud.ResourceGroup
+	}
+
+	accountKey, err := GetStorageAccesskey(d.cloud, accountName, resourceGroupName)
+	if err != nil {
+		return nil, fmt.Errorf("no key for storage account(%s) under resource group(%s), err %v", accountName, resourceGroupName, err)
+	}
+
+	klog.V(2).Infof("deleting container(%s) rg(%s) account(%s) volumeID(%s)", containerName, resourceGroupName, accountName, volumeID)
+	client, err := azstorage.NewBasicClientOnSovereignCloud(accountName, accountKey, d.cloud.Environment)
+	if err != nil {
+		return nil, err
+	}
+	blobClient := client.GetBlobService()
+	container := blobClient.GetContainerReference(containerName)
+	// todo: check what value to add into DeleteContainerOptions
+	_, err = container.DeleteIfExists(nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to delete container(%s) on account(%s), error: %v", containerName, accountName, err)
+	}
+
+	klog.V(2).Infof("container(%s) under rg(%s) account(%s) volumeID(%s) is deleted successfully", containerName, resourceGroupName, accountName, volumeID)
+
+	return &csi.DeleteVolumeResponse{}, nil
+}
+
+// ValidateVolumeCapabilities return the capabilities of the volume
+func (d *Driver) ValidateVolumeCapabilities(ctx context.Context, req *csi.ValidateVolumeCapabilitiesRequest) (*csi.ValidateVolumeCapabilitiesResponse, error) {
+	if len(req.GetVolumeId()) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Volume ID missing in request")
+	}
+	if req.GetVolumeCapabilities() == nil {
+		return nil, status.Error(codes.InvalidArgument, "Volume capabilities missing in request")
+	}
+
+	// todo: we may check file share existence here
+
+	// blobfuse supports all AccessModes, no need to check capabilities here
+	return &csi.ValidateVolumeCapabilitiesResponse{Message: ""}, nil
+}
+
+// ControllerGetCapabilities returns the capabilities of the Controller plugin
+func (d *Driver) ControllerGetCapabilities(ctx context.Context, req *csi.ControllerGetCapabilitiesRequest) (*csi.ControllerGetCapabilitiesResponse, error) {
+	klog.V(5).Infof("Using default ControllerGetCapabilities")
+
+	return &csi.ControllerGetCapabilitiesResponse{
+		Capabilities: d.Cap,
+	}, nil
+}
+
+// GetCapacity returns the capacity of the total available storage pool
+func (d *Driver) GetCapacity(ctx context.Context, req *csi.GetCapacityRequest) (*csi.GetCapacityResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "")
+}
+
+// ListVolumes return all available volumes
+func (d *Driver) ListVolumes(ctx context.Context, req *csi.ListVolumesRequest) (*csi.ListVolumesResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "")
+}
+
+// ControllerPublishVolume make a volume available on some required node
+// N/A for blobfuse
+func (d *Driver) ControllerPublishVolume(ctx context.Context, req *csi.ControllerPublishVolumeRequest) (*csi.ControllerPublishVolumeResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "")
+}
+
+// ControllerUnpublishVolume make the volume unavailable on a specified node
+// N/A for blobfuse
+func (d *Driver) ControllerUnpublishVolume(ctx context.Context, req *csi.ControllerUnpublishVolumeRequest) (*csi.ControllerUnpublishVolumeResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "")
+}
+
+// CreateSnapshot create a snapshot (todo)
+func (d *Driver) CreateSnapshot(ctx context.Context, req *csi.CreateSnapshotRequest) (*csi.CreateSnapshotResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "")
+}
+
+// DeleteSnapshot delete a snapshot (todo)
+func (d *Driver) DeleteSnapshot(ctx context.Context, req *csi.DeleteSnapshotRequest) (*csi.DeleteSnapshotResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "")
+}
+
+// ListSnapshots list all snapshots (todo)
+func (d *Driver) ListSnapshots(ctx context.Context, req *csi.ListSnapshotsRequest) (*csi.ListSnapshotsResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "")
+}
---
+++ vendor/github.com/csi-driver/blobfuse-csi-driver/pkg/blobfuse/nodeserver.go
@@ -0,0 +1,224 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package blobfuse
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/container-storage-interface/spec/lib/go/csi"
+	"k8s.io/klog"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"golang.org/x/net/context"
+)
+
+// NodePublishVolume mount the volume from staging to target path
+func (d *Driver) NodePublishVolume(ctx context.Context, req *csi.NodePublishVolumeRequest) (*csi.NodePublishVolumeResponse, error) {
+	if req.GetVolumeCapability() == nil {
+		return nil, status.Error(codes.InvalidArgument, "Volume capability missing in request")
+	}
+	if len(req.GetVolumeId()) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Volume ID missing in request")
+	}
+	if len(req.GetTargetPath()) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Target path missing in request")
+	}
+
+	targetPath := req.GetTargetPath()
+	notMnt, err := d.mounter.IsLikelyNotMountPoint(targetPath)
+	if err != nil && !os.IsNotExist(err) {
+		return nil, err
+	}
+
+	if err := d.mounter.MakeDir(targetPath); err != nil {
+		return nil, err
+	}
+
+	if !notMnt {
+		// testing original mount point, make sure the mount link is valid
+		if _, err := ioutil.ReadDir(targetPath); err == nil {
+			klog.V(2).Infof("azureFile - already mounted to target %s", targetPath)
+			return &csi.NodePublishVolumeResponse{}, nil
+		}
+		// todo: mount link is invalid, now unmount and remount later (built-in functionality)
+		klog.Warningf("azureFile - ReadDir %s failed with %v, unmount this directory", targetPath, err)
+		if err := d.mounter.Unmount(targetPath); err != nil {
+			klog.Errorf("azureFile - Unmount directory %s failed with %v", targetPath, err)
+			return nil, err
+		}
+		// notMnt = true
+	}
+
+	fsType := req.GetVolumeCapability().GetMount().GetFsType()
+
+	readOnly := req.GetReadonly()
+	volumeID := req.GetVolumeId()
+	attrib := req.GetVolumeContext()
+	mountFlags := req.GetVolumeCapability().GetMount().GetMountFlags()
+
+	klog.V(2).Infof("target %v\nfstype %v\n\nreadonly %v\nvolumeId %v\ncontext %v\nmountflags %v\n",
+		targetPath, fsType, readOnly, volumeID, attrib, mountFlags)
+
+	var accountName, accountKey, containerName string
+
+	secrets := req.GetSecrets()
+	if len(secrets) == 0 {
+		var resourceGroupName string
+		resourceGroupName, accountName, containerName, err = getContainerInfo(volumeID)
+		if err != nil {
+			return nil, err
+		}
+
+		if resourceGroupName == "" {
+			resourceGroupName = d.cloud.ResourceGroup
+		}
+
+		accountKey, err = GetStorageAccesskey(d.cloud, accountName, resourceGroupName)
+		if err != nil {
+			return nil, fmt.Errorf("no key for storage account(%s) under resource group(%s), err %v", accountName, resourceGroupName, err)
+		}
+	} else {
+		for k, v := range attrib {
+			switch strings.ToLower(k) {
+			case "containername":
+				containerName = v
+			}
+		}
+		if containerName == "" {
+			return nil, fmt.Errorf("could not find containerName from attributes(%v)", attrib)
+		}
+
+		accountName, accountKey, err = getStorageAccount(secrets)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// todo: use could set tmp-path and other mountOptions
+	cmd := exec.Command("/usr/bin/blobfuse", targetPath, "--tmp-path=/tmp/blobfuse/", "--container-name="+containerName)
+	cmd.Env = append(os.Environ(), "AZURE_STORAGE_ACCOUNT="+accountName, "AZURE_STORAGE_ACCESS_KEY="+accountKey)
+	err = cmd.Run()
+	if err != nil {
+		notMnt, mntErr := d.mounter.IsLikelyNotMountPoint(targetPath)
+		if mntErr != nil {
+			klog.Errorf("IsLikelyNotMountPoint check failed: %v", mntErr)
+			return nil, err
+		}
+		if !notMnt {
+			if mntErr = d.mounter.Unmount(targetPath); mntErr != nil {
+				klog.Errorf("Failed to unmount: %v", mntErr)
+				return nil, err
+			}
+			notMnt, mntErr := d.mounter.IsLikelyNotMountPoint(targetPath)
+			if mntErr != nil {
+				klog.Errorf("IsLikelyNotMountPoint check failed: %v", mntErr)
+				return nil, err
+			}
+			if !notMnt {
+				// This is very odd, we don't expect it.  We'll try again next sync loop.
+				klog.Errorf("%s is still mounted, despite call to unmount().  Will try again next sync loop.", targetPath)
+				return nil, err
+			}
+		}
+		os.Remove(targetPath)
+		return nil, err
+	}
+
+	return &csi.NodePublishVolumeResponse{}, nil
+}
+
+// NodeUnpublishVolume unmount the volume from the target path
+func (d *Driver) NodeUnpublishVolume(ctx context.Context, req *csi.NodeUnpublishVolumeRequest) (*csi.NodeUnpublishVolumeResponse, error) {
+	if len(req.GetVolumeId()) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Volume ID missing in request")
+	}
+	if len(req.GetTargetPath()) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Target path missing in request")
+	}
+	targetPath := req.GetTargetPath()
+	volumeID := req.GetVolumeId()
+
+	// Unmounting the image
+	err := d.mounter.Unmount(req.GetTargetPath())
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	klog.V(4).Infof("blobfuse: volume %s/%s has been unmounted.", targetPath, volumeID)
+
+	return &csi.NodeUnpublishVolumeResponse{}, nil
+}
+
+// NodeStageVolume mount the volume to a staging path
+// todo: we may implement this for blobfuse
+// The reason that mounting is a two step operation is
+// because Kubernetes allows you to use a single volume by multiple pods.
+// This is allowed when the storage system supports it or if all pods run on the same node.
+func (d *Driver) NodeStageVolume(ctx context.Context, req *csi.NodeStageVolumeRequest) (*csi.NodeStageVolumeResponse, error) {
+	if len(req.GetVolumeId()) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Volume ID missing in request")
+	}
+	if len(req.GetStagingTargetPath()) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Target path missing in request")
+	}
+
+	return &csi.NodeStageVolumeResponse{}, nil
+}
+
+// NodeUnstageVolume unmount the volume from the staging path
+// todo: we may implement this for blobfuse
+// The reason that mounting is a two step operation is
+// because Kubernetes allows you to use a single volume by multiple pods.
+// This is allowed when the storage system supports it or if all pods run on the same node.
+func (d *Driver) NodeUnstageVolume(ctx context.Context, req *csi.NodeUnstageVolumeRequest) (*csi.NodeUnstageVolumeResponse, error) {
+	if len(req.GetVolumeId()) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Volume ID missing in request")
+	}
+	if len(req.GetStagingTargetPath()) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Target path missing in request")
+	}
+
+	return &csi.NodeUnstageVolumeResponse{}, nil
+}
+
+// NodeGetCapabilities return the capabilities of the Node plugin
+func (d *Driver) NodeGetCapabilities(ctx context.Context, req *csi.NodeGetCapabilitiesRequest) (*csi.NodeGetCapabilitiesResponse, error) {
+	klog.V(2).Infof("Using default NodeGetCapabilities")
+
+	return &csi.NodeGetCapabilitiesResponse{
+		Capabilities: d.NSCap,
+	}, nil
+}
+
+// NodeGetInfo return info of the node on which this plugin is running
+func (d *Driver) NodeGetInfo(ctx context.Context, req *csi.NodeGetInfoRequest) (*csi.NodeGetInfoResponse, error) {
+	klog.V(5).Infof("Using default NodeGetInfo")
+
+	return &csi.NodeGetInfoResponse{
+		NodeId: d.NodeID,
+	}, nil
+}
+
+// NodeGetVolumeStats get volume stats
+func (d *Driver) NodeGetVolumeStats(ctx context.Context, in *csi.NodeGetVolumeStatsRequest) (*csi.NodeGetVolumeStatsResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "")
+}
