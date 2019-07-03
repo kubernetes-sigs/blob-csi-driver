44fa73917d2ae9d00f3ce3abc4151ae91e3af456
andyzhangx <xiazhang@microsoft.com>
2019-07-03 13:43:46 +0000

    chore: enable mount/unmount CI test

--- test/integration/run-test.sh
+++ test/integration/run-test.sh
@@ -63,7 +63,7 @@ if [ -v aadClientSecret ]; then
 		exit $retcode
 	fi
 
-	if [ "$cloud" == "NO-RUN-AzureChinaCloud" ]; then
+	if [ "$cloud" != "AzureChinaCloud" ]; then
 		# blobfuse mount/unmount on travis VM does not work against AzureChinaCloud
 		echo "mount volume test:"
 		$csc node publish --endpoint $endpoint --cap 1,block --target-path $target_path $volumeid
