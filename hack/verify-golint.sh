718d9f43dddc0f8917f8d31161396da796b0f892
andyzhangx <xiazhang@microsoft.com>
2020-04-23 02:29:29 +0000

    test: fix golint issue

--- hack/verify-golint.sh
+++ hack/verify-golint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2019 The Kubernetes Authors.
+# Copyright 2018 The Kubernetes Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,11 +16,11 @@
 
 set -euo pipefail
 
-if ! which golangci-lint > /dev/null; then
-    echo "Cannot find golangci-lint. Installing golangci-lint..."
-    go get github.com/golangci/golangci-lint/cmd/golangci-lint
+if [[ -z "$(command -v golangci-lint)" ]]; then
+  echo "Cannot find golangci-lint. Installing golangci-lint..."
+  curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.24.0
 fi
 
-golangci-lint run --deadline=10m
+golangci-lint run --no-config --enable=golint --disable=typecheck --deadline=10m
 
-echo "Congratulations! All Go source files have been linted."
+echo "Congratulations! Lint check completed for all Go source files."
