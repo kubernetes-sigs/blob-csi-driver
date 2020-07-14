b23e4177448591cdb92fc604c84156994e028f1d
Sakuralbj <sakuranlbj@gmail.com>
2020-07-14 11:40:38 +0800

    test: add hack/verify-gomod.sh

--- hack/verify-all.sh
+++ hack/verify-all.sh
@@ -21,6 +21,7 @@ PKG_ROOT=$(git rev-parse --show-toplevel)
 ${PKG_ROOT}/hack/verify-gofmt.sh
 ${PKG_ROOT}/hack/verify-govet.sh
 ${PKG_ROOT}/hack/verify-golint.sh
+${PKG_ROOT}/hack/verify-gomod.sh
 ${PKG_ROOT}/hack/verify-yamllint.sh
 ${PKG_ROOT}/hack/verify-boilerplate.sh
 ${PKG_ROOT}/hack/verify-spelling.sh
