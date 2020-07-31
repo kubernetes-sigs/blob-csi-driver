eaf3b61db5ca3b9575bba7bdca955d786e41bf77
Andy Zhang <xiazhang@microsoft.com>
2020-07-31 21:41:23 +0800

    Update release-image.sh

--- hack/release-image.sh
+++ hack/release-image.sh
@@ -26,6 +26,7 @@ export REGISTRY=$REGISTRY_NAME.azurecr.io
 export IMAGE_NAME=public/k8s/csi/blobfuse-csi
 export CI=1
 export PUBLISH=1
+az acr login --name $REGISTRY_NAME
 make blobfuse-container
 make push
 make push-latest
