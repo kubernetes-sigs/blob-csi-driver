427f266e03a347203e0f0c5843b7f9e9e9a03924
andyzhangx <xiazhang@microsoft.com>
2022-04-24 12:33:30 +0000

    chore: append secretNamespace to volumeid
    chore: append secretNamespace to volumeid
    
    fix

--- pkg/blob/blob_test.go
+++ pkg/blob/blob_test.go
@@ -168,62 +168,88 @@ func TestRun(t *testing.T) {
 
 func TestGetContainerInfo(t *testing.T) {
 	tests := []struct {
-		options   string
-		expected1 string
-		expected2 string
-		expected3 string
-		expected4 error
+		volumeID      string
+		rg            string
+		account       string
+		container     string
+		namespace     string
+		expectedError error
 	}{
 		{
-			options:   "rg#f5713de20cde511e8ba4900#pvc-file-dynamic-17e43f84-f474-11e8-acd0-000d3a00df41#uuid",
-			expected1: "rg",
-			expected2: "f5713de20cde511e8ba4900",
-			expected3: "pvc-file-dynamic-17e43f84-f474-11e8-acd0-000d3a00df41",
-			expected4: nil,
+			volumeID:      "rg#f5713de20cde511e8ba4900#container#uuid#namespace",
+			rg:            "rg",
+			account:       "f5713de20cde511e8ba4900",
+			container:     "container",
+			namespace:     "namespace",
+			expectedError: nil,
 		},
 		{
-			options:   "rg#f5713de20cde511e8ba4900#pvc-file-dynamic-17e43f84-f474-11e8-acd0-000d3a00df41#",
-			expected1: "rg",
-			expected2: "f5713de20cde511e8ba4900",
-			expected3: "pvc-file-dynamic-17e43f84-f474-11e8-acd0-000d3a00df41",
-			expected4: nil,
+			volumeID:      "rg#f5713de20cde511e8ba4900#container##namespace",
+			rg:            "rg",
+			account:       "f5713de20cde511e8ba4900",
+			container:     "container",
+			namespace:     "namespace",
+			expectedError: nil,
 		},
 		{
-			options:   "rg#f5713de20cde511e8ba4900#pvc-file-dynamic-17e43f84-f474-11e8-acd0-000d3a00df41",
-			expected1: "rg",
-			expected2: "f5713de20cde511e8ba4900",
-			expected3: "pvc-file-dynamic-17e43f84-f474-11e8-acd0-000d3a00df41",
-			expected4: nil,
+			volumeID:      "rg#f5713de20cde511e8ba4900#container##",
+			rg:            "rg",
+			account:       "f5713de20cde511e8ba4900",
+			container:     "container",
+			namespace:     "",
+			expectedError: nil,
 		},
 		{
-			options:   "rg#f5713de20cde511e8ba4900",
-			expected1: "",
-			expected2: "",
-			expected3: "",
-			expected4: fmt.Errorf("error parsing volume id: \"rg#f5713de20cde511e8ba4900\", should at least contain two #"),
+			volumeID:      "rg#f5713de20cde511e8ba4900#pvc-file-dynamic-17e43f84-f474-11e8-acd0-000d3a00df41#uuid",
+			rg:            "rg",
+			account:       "f5713de20cde511e8ba4900",
+			container:     "pvc-file-dynamic-17e43f84-f474-11e8-acd0-000d3a00df41",
+			expectedError: nil,
 		},
 		{
-			options:   "rg",
-			expected1: "",
-			expected2: "",
-			expected3: "",
-			expected4: fmt.Errorf("error parsing volume id: \"rg\", should at least contain two #"),
+			volumeID:      "rg#f5713de20cde511e8ba4900#pvc-file-dynamic-17e43f84-f474-11e8-acd0-000d3a00df41#",
+			rg:            "rg",
+			account:       "f5713de20cde511e8ba4900",
+			container:     "pvc-file-dynamic-17e43f84-f474-11e8-acd0-000d3a00df41",
+			expectedError: nil,
 		},
 		{
-			options:   "",
-			expected1: "",
-			expected2: "",
-			expected3: "",
-			expected4: fmt.Errorf("error parsing volume id: \"\", should at least contain two #"),
+			volumeID:      "rg#f5713de20cde511e8ba4900#pvc-file-dynamic-17e43f84-f474-11e8-acd0-000d3a00df41",
+			rg:            "rg",
+			account:       "f5713de20cde511e8ba4900",
+			container:     "pvc-file-dynamic-17e43f84-f474-11e8-acd0-000d3a00df41",
+			expectedError: nil,
+		},
+		{
+			volumeID:      "rg#f5713de20cde511e8ba4900",
+			rg:            "",
+			account:       "",
+			container:     "",
+			expectedError: fmt.Errorf("error parsing volume id: \"rg#f5713de20cde511e8ba4900\", should at least contain two #"),
+		},
+		{
+			volumeID:      "rg",
+			rg:            "",
+			account:       "",
+			container:     "",
+			expectedError: fmt.Errorf("error parsing volume id: \"rg\", should at least contain two #"),
+		},
+		{
+			volumeID:      "",
+			rg:            "",
+			account:       "",
+			container:     "",
+			expectedError: fmt.Errorf("error parsing volume id: \"\", should at least contain two #"),
 		},
 	}
 
 	for _, test := range tests {
-		result1, result2, result3, result4 := GetContainerInfo(test.options)
-		if !reflect.DeepEqual(result1, test.expected1) || !reflect.DeepEqual(result2, test.expected2) ||
-			!reflect.DeepEqual(result3, test.expected3) || !reflect.DeepEqual(result4, test.expected4) {
-			t.Errorf("input: %q, GetContainerInfo result1: %q, expected1: %q, result2: %q, expected2: %q, result3: %q, expected3: %q, result4: %q, expected4: %q", test.options, result1, test.expected1, result2, test.expected2,
-				result3, test.expected3, result4, test.expected4)
+		rg, account, container, ns, err := GetContainerInfo(test.volumeID)
+		if !reflect.DeepEqual(rg, test.rg) || !reflect.DeepEqual(account, test.account) ||
+			!reflect.DeepEqual(container, test.container) || !reflect.DeepEqual(err, test.expectedError) ||
+			!reflect.DeepEqual(ns, test.namespace) {
+			t.Errorf("input: %q, GetContainerInfo rg: %q, rg: %q, account: %q, account: %q, container: %q, container: %q, namespace: %q, namespace: %q, err: %q, expectedError: %q", test.volumeID, rg, test.rg, account, test.account,
+				container, test.container, ns, test.namespace, err, test.expectedError)
 		}
 	}
 }
