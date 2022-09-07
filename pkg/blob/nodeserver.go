22974bd30326d00fea73ac0033d317ee1a520fb8
weizhichen <weizhichen@microsoft.com>
2022-09-07 10:51:19 +0000

    remove AZURE_STORAGE_BLOB_ENDPOINT

--- pkg/blob/nodeserver.go
+++ pkg/blob/nodeserver.go
@@ -336,7 +336,7 @@ func (d *Driver) NodeStageVolume(ctx context.Context, req *csi.NodeStageVolumeRe
 	klog.V(2).Infof("target %v\nprotocol %v\n\nvolumeId %v\ncontext %v\nmountflags %v\nmountOptions %v\nargs %v\nserverAddress %v",
 		targetPath, protocol, volumeID, attrib, mountFlags, mountOptions, args, serverAddress)
 
-	authEnv = append(authEnv, "AZURE_STORAGE_ACCOUNT="+accountName, "AZURE_STORAGE_BLOB_ENDPOINT="+serverAddress)
+	authEnv = append(authEnv, "AZURE_STORAGE_ACCOUNT="+accountName)
 	if d.enableBlobMockMount {
 		klog.Warningf("NodeStageVolume: mock mount on volumeID(%s), this is only for TESTING!!!", volumeID)
 		if err := volumehelper.MakeDir(targetPath, os.FileMode(mountPermissions)); err != nil {
