75c5ba5be6b29c0241b27c383282cda0039ae0d8
weizhichen <weizhichen@microsoft.com>
2023-01-07 07:19:10 +0000

    add e2e test for unmount deleted volume

--- test/e2e/testsuites/specs.go
+++ test/e2e/testsuites/specs.go
@@ -148,7 +148,7 @@ func (pod *PodDetails) SetupWithPreProvisionedVolumes(client clientset.Interface
 	return tpod, cleanupFuncs
 }
 
-func (pod *PodDetails) SetupDeployment(client clientset.Interface, namespace *v1.Namespace, csiDriver driver.DynamicPVTestDriver, storageClassParameters map[string]string) (*TestDeployment, []func()) {
+func (pod *PodDetails) SetupDeployment(client clientset.Interface, namespace *v1.Namespace, csiDriver driver.DynamicPVTestDriver, storageClassParameters map[string]string) (*TestDeployment, []func(), string) {
 	cleanupFuncs := make([]func(), 0)
 	volume := pod.Volumes[0]
 	ginkgo.By("setting up the StorageClass")
@@ -166,7 +166,11 @@ func (pod *PodDetails) SetupDeployment(client clientset.Interface, namespace *v1
 	tDeployment := NewTestDeployment(client, namespace, pod.Cmd, tpvc.persistentVolumeClaim, fmt.Sprintf("%s%d", volume.VolumeMount.NameGenerate, 1), fmt.Sprintf("%s%d", volume.VolumeMount.MountPathGenerate, 1), volume.VolumeMount.ReadOnly)
 
 	cleanupFuncs = append(cleanupFuncs, tDeployment.Cleanup)
-	return tDeployment, cleanupFuncs
+	var volumeID string
+	if tpvc.persistentVolume != nil && tpvc.persistentVolume.Spec.CSI != nil {
+		volumeID = tpvc.persistentVolume.Spec.CSI.VolumeHandle
+	}
+	return tDeployment, cleanupFuncs, volumeID
 }
 
 func (volume *VolumeDetails) SetupDynamicPersistentVolumeClaim(client clientset.Interface, namespace *v1.Namespace, csiDriver driver.DynamicPVTestDriver, storageClassParameters map[string]string) (*TestPersistentVolumeClaim, []func()) {
