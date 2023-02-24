4365dd9d39764b369b559d5f70929047ae5c15af
andyzhangx <xiazhang@microsoft.com>
2023-02-24 03:48:24 +0000

    [release-1.19] fix: mount failed on ubuntu 22.04(aks 1.25.2)

--- pkg/blobfuse-proxy/server/server.go
+++ pkg/blobfuse-proxy/server/server.go
@@ -71,6 +71,12 @@ func (server *MountServer) MountAzureBlob(ctx context.Context,
 	if protocol == blob.Fuse2 || server.blobfuseVersion == BlobfuseV2 {
 		klog.V(2).Infof("using blobfuse V2 to mount")
 		args = "mount " + args
+		// add this arg for blobfuse2 to solve the issue:
+		// https://github.com/Azure/azure-storage-fuse/issues/1015
+		if !strings.Contains(args, "--ignore-open-flags") {
+			klog.V(2).Infof("append --ignore-open-flags=true to mount args")
+			args = args + " " + "--ignore-open-flags=true"
+		}
 		cmd = exec.Command("blobfuse2", strings.Split(args, " ")...)
 	} else {
 		klog.V(2).Infof("using blobfuse V1 to mount")
