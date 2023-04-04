7386e44ef7a509e601584b1e573dbfc760569a05
weizhichen <weizhichen@microsoft.com>
2023-04-04 22:36:48 +0000

    fix bash

--- test/utils/restart_driver_daemonset.sh
+++ test/utils/restart_driver_daemonset.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -euo pipefail
+set -eo pipefail
 
 echo "*****************start blob log (before restart)***********************"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
