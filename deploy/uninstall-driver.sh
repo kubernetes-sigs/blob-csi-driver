15e58374f61ece3234abe6065106b1c61e714893
weizhichen <weizhichen@microsoft.com>
2023-06-12 08:01:03 +0000

    fix uninstall-driver to make it backward compatibility

--- deploy/uninstall-driver.sh
+++ deploy/uninstall-driver.sh
@@ -33,6 +33,13 @@ if [ $ver != "master" ]; then
   repo="$repo/$ver"
 fi
 
+if [[ "$#" -gt 1 ]]; then
+  if [[ "$2" == *"blobfuse-proxy"* ]]; then
+    echo "remove blobfuse-proxy daemonset ..."
+    kubectl delete daemonset csi-blobfuse-proxy -n kube-system --ignore-not-found
+  fi
+fi
+
 
 echo "Uninstalling Azure Blob Storage CSI driver, version: $ver ..."
 kubectl delete -f $repo/csi-blob-controller.yaml --ignore-not-found
