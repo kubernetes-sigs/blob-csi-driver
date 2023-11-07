fd67c9d8c59aa0906a03404acafbf2ed7b11c9fb
weizhichen <weizhichen@microsoft.com>
2023-11-07 07:28:08 +0000

    Squashed commit of the following:
    commit 0c185e7a63b7cc88efa217af205cd0871149abdd
    Merge: bd1e02898 3f47b3ea1
    Author: weizhichen <weizhichen@microsoft.com>
    Date:   Mon Nov 6 05:27:11 2023 +0000
    
        Merge branch 'master' of https://github.com/kubernetes-sigs/blob-csi-driver into support-openshift
    
    commit bd1e028985139bbc3eb7cdac7300be1d8188bf34
    Author: weizhichen <weizhichen@microsoft.com>
    Date:   Wed Oct 18 08:40:28 2023 +0000
    
        fix
    
    commit 6270febadec81048ffd7c610b373595de5a60b96
    Merge: 697f9ab79 b99e1cf51
    Author: weizhichen <weizhichen@microsoft.com>
    Date:   Wed Oct 18 07:30:33 2023 +0000
    
        Merge branch 'master' of https://github.com/kubernetes-sigs/blob-csi-driver into support-openshift
    
    commit 697f9ab794f178ceb6164225a6cbb0f2aa8aca2c
    Author: weizhichen <weizhichen@microsoft.com>
    Date:   Mon Oct 16 08:07:59 2023 +0000
    
        arm
    
    commit c79dc0f2b6560e19c6bdcfc5460db746095f98af
    Author: weizhichen <weizhichen@microsoft.com>
    Date:   Mon Oct 16 07:16:45 2023 +0000
    
        fix
    
    commit 7096aa8206f772f76e63ecac593c3b2cd0aa9dad
    Author: weizhichen <weizhichen@microsoft.com>
    Date:   Mon Oct 16 07:06:17 2023 +0000
    
        fix
    
    commit 617ea0e27561e088db99230618f3f89680379091
    Author: weizhichen <weizhichen@microsoft.com>
    Date:   Mon Oct 16 06:48:07 2023 +0000
    
        add mariner
    
    commit 2f4473cb715dfefa577786a75b53a8fc4c72270a
    Author: weizhichen <weizhichen@microsoft.com>
    Date:   Mon Oct 16 06:06:48 2023 +0000
    
        fix
    
    commit ee5e239784273c86a3370f63c4a7e3c4a61fc159
    Author: weizhichen <weizhichen@microsoft.com>
    Date:   Mon Oct 16 06:01:50 2023 +0000
    
        fix
    
    commit fd8e1f5df19e329d79bbd948d523e59af1ba39d5
    Author: weizhichen <weizhichen@microsoft.com>
    Date:   Mon Oct 16 05:47:02 2023 +0000
    
        fix
    
    commit babe2f0a84e98a8478b060b9df5f5769cd41e9a6
    Author: weizhichen <weizhichen@microsoft.com>
    Date:   Mon Oct 16 05:44:54 2023 +0000
    
        fix
    
    commit 6ce12c847617d74f97f2a785e504f2cdadb1e011
    Author: weizhichen <weizhichen@microsoft.com>
    Date:   Mon Oct 16 04:17:51 2023 +0000
    
        support openshift
    
    commit 38f50a9e908174ad392ac6f8692a5f22e9ec6dab
    Author: weizhichen <weizhichen@microsoft.com>
    Date:   Mon Oct 16 02:48:57 2023 +0000
    
        Revert "support openshift"
    
        This reverts commit e15df3356bb2b5c956f8c4f944562ad02c906357.
    
    commit e15df3356bb2b5c956f8c4f944562ad02c906357
    Author: weizhichen <weizhichen@microsoft.com>
    Date:   Fri Oct 13 07:27:24 2023 +0000
    
        support openshift

---
+++ pkg/blobfuse-proxy/install-proxy-rhcos.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+# Copyright 2019 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -xe
+
+# in coreos, we could just copy the blobfuse2 binary to /usr/local/bin/blobfuse2
+if [ "${INSTALL_BLOBFUSE}" = "true" ] || [ "${INSTALL_BLOBFUSE2}" = "true" ]
+then
+  echo "copy blobfuse2...."
+  cp /usr/bin/blobfuse2 /host/usr/local/bin/blobfuse2
+fi
+
+# install blobfuse-proxy
+updateBlobfuseProxy="true"
+if [ -f "/host/usr/local/bin/blobfuse-proxy" ];then
+  old=$(sha256sum /host/usr/local/bin/blobfuse-proxy | awk '{print $1}')
+  new=$(sha256sum /blobfuse-proxy/blobfuse-proxy | awk '{print $1}')
+  if [ "$old" = "$new" ];then
+    updateBlobfuseProxy="false"
+    echo "no need to update blobfuse-proxy"
+  fi
+fi
+if [ "$updateBlobfuseProxy" = "true" ];then
+  echo "copy blobfuse-proxy...."
+  rm -rf /host/var/lib/kubelet/plugins/blob.csi.azure.com/blobfuse-proxy.sock
+  rm -rf /host/usr/local/bin/blobfuse-proxy
+  cp /blobfuse-proxy/blobfuse-proxy /host/usr/local/bin/blobfuse-proxy
+  chmod 755 /host/usr/local/bin/blobfuse-proxy
+fi
+
+updateService="true"
+echo "change from /usr/bin/blobfuse-proxy to /usr/local/bin/blobfuse-proxy in blobfuse-proxy.service"
+sed -i 's/\/usr\/bin\/blobfuse-proxy/\/usr\/local\/bin\/blobfuse-proxy/g' /blobfuse-proxy/blobfuse-proxy.service
+if [ -f "/host/etc/systemd/system/blobfuse-proxy.service" ];then
+  old=$(sha256sum /host/etc/systemd/system/blobfuse-proxy.service | awk '{print $1}')
+  new=$(sha256sum /blobfuse-proxy/blobfuse-proxy.service | awk '{print $1}')
+  if [ "$old" = "$new" ];then
+      updateService="false"
+      echo "no need to update blobfuse-proxy.service"
+  fi
+fi
+if [ "$updateService" = "true" ];then
+  echo "copy blobfuse-proxy.service...."
+  mkdir -p /host/etc/systemd/system/
+  cp /blobfuse-proxy/blobfuse-proxy.service /host/etc/systemd/system/blobfuse-proxy.service
+fi
+
+if [ "${INSTALL_BLOBFUSE_PROXY}" = "true" ];then
+  if [ "$updateBlobfuseProxy" = "true" ] || [ "$updateService" = "true" ];then
+    echo "start blobfuse-proxy...."
+    $HOST_CMD systemctl daemon-reload
+    $HOST_CMD systemctl enable blobfuse-proxy.service
+    $HOST_CMD systemctl restart blobfuse-proxy.service
+  fi
+fi
---
+++ pkg/blobfuse-proxy/install-proxy-ubuntu.sh
@@ -0,0 +1,117 @@
+#!/bin/sh
+
+# Copyright 2019 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -xe
+
+# install blobfuse/blobfuse2
+if [ "${ARCH}" = "aarch64" ]
+then
+  echo "skip install blobfuse/blobfuse2 for arm64...."
+elif [ "${INSTALL_BLOBFUSE}" = "true" ] || [ "${INSTALL_BLOBFUSE2}" = "true" ]
+then
+  echo "start to install blobfuse/blobfuse2...."
+
+  release=$($HOST_CMD lsb_release -rs)
+  echo "Ubuntu release: $release"
+  
+  if [ "$(expr "$release" \< "22.04")" -eq 1 ]
+  then
+    cp /blobfuse-proxy/packages-microsoft-prod-18.04.deb /host/etc/packages-microsoft-prod.deb
+  else
+    cp /blobfuse-proxy/packages-microsoft-prod-22.04.deb /host/etc/packages-microsoft-prod.deb
+  fi
+  # when running dpkg -i /etc/packages-microsoft-prod.deb, need to enter y to continue. 
+  # refer to https://stackoverflow.com/questions/45349571/how-to-install-deb-with-dpkg-non-interactively
+  yes | $HOST_CMD dpkg -i /etc/packages-microsoft-prod.deb && $HOST_CMD apt update
+
+  pkg_list=""
+  # blobfuse
+  if [ "${INSTALL_BLOBFUSE}" = "true" ] && [ "$(expr "$release" \< "22.04")" -eq 1 ]
+  then
+    pkg_list="${pkg_list} fuse"
+    if [ -z "${BLOBFUSE_VERSION}" ]; then
+      echo "install blobfuse with latest version"
+      pkg_list="${pkg_list} blobfuse"
+    else
+      pkg_list="${pkg_list} blobfuse=${BLOBFUSE_VERSION}"
+    fi
+  fi
+
+  # blobfuse2
+  if [ "${INSTALL_BLOBFUSE2}" = "true" ]
+  then
+    if [ "$(expr "$release" \< "22.04")" -eq 1 ]; then
+      echo "install fuse for blobfuse2"
+      pkg_list="${pkg_list} fuse"
+    else
+      echo "install fuse3 for blobfuse2, current release is $release"
+      pkg_list="${pkg_list} fuse3"
+    fi
+
+    if [ -z "${BLOBFUSE2_VERSION}" ]; then
+      echo "install blobfuse2 with latest version"
+      pkg_list="${pkg_list} blobfuse2"
+    else
+      pkg_list="${pkg_list} blobfuse2=${BLOBFUSE2_VERSION}"
+    fi
+  fi
+
+  echo "begin to install ${pkg_list}"
+  $HOST_CMD apt-get install -y $pkg_list
+  $HOST_CMD rm -f /etc/packages-microsoft-prod.deb
+fi
+
+# install blobfuse-proxy
+updateBlobfuseProxy="true"
+if [ -f "/host/usr/bin/blobfuse-proxy" ];then
+  old=$(sha256sum /host/usr/bin/blobfuse-proxy | awk '{print $1}')
+  new=$(sha256sum /blobfuse-proxy/blobfuse-proxy | awk '{print $1}')
+  if [ "$old" = "$new" ];then
+    updateBlobfuseProxy="false"
+    echo "no need to update blobfuse-proxy"
+  fi
+fi
+if [ "$updateBlobfuseProxy" = "true" ];then
+  echo "copy blobfuse-proxy...."
+  rm -rf /host/var/lib/kubelet/plugins/blob.csi.azure.com/blobfuse-proxy.sock
+  rm -rf /host/usr/bin/blobfuse-proxy
+  cp /blobfuse-proxy/blobfuse-proxy /host/usr/bin/blobfuse-proxy
+  chmod 755 /host/usr/bin/blobfuse-proxy
+fi
+
+updateService="true"
+if [ -f "/host/usr/lib/systemd/system/blobfuse-proxy.service" ];then
+  old=$(sha256sum /host/usr/lib/systemd/system/blobfuse-proxy.service | awk '{print $1}')
+  new=$(sha256sum /blobfuse-proxy/blobfuse-proxy.service | awk '{print $1}')
+  if [ "$old" = "$new" ];then
+      updateService="false"
+      echo "no need to update blobfuse-proxy.service"
+  fi
+fi
+if [ "$updateService" = "true" ];then
+  echo "copy blobfuse-proxy.service...."
+  mkdir -p /host/usr/lib/systemd/system
+  cp /blobfuse-proxy/blobfuse-proxy.service /host/usr/lib/systemd/system/blobfuse-proxy.service
+fi
+
+if [ "${INSTALL_BLOBFUSE_PROXY}" = "true" ];then
+  if [ "$updateBlobfuseProxy" = "true" ] || [ "$updateService" = "true" ];then
+    echo "start blobfuse-proxy...."
+    $HOST_CMD systemctl daemon-reload
+    $HOST_CMD systemctl enable blobfuse-proxy.service
+    $HOST_CMD systemctl restart blobfuse-proxy.service
+  fi
+fi
