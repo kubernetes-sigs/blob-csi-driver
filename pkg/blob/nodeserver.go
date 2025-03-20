93cdc967f47bafbab504d29310e8c7b1dd684cf2
Anya Chang <anyachang@microsoft.com>
2025-03-19 19:36:22 -0700

    fix mismatch gid on pod and volume

--- pkg/blob/nodeserver.go
+++ pkg/blob/nodeserver.go
@@ -392,7 +392,7 @@ func (d *Driver) NodeStageVolume(ctx context.Context, req *csi.NodeStageVolumeRe
 			if err != nil {
 				return nil, status.Error(codes.InvalidArgument, fmt.Sprintf("convert %s to int failed with %v", volumeMountGroup, err))
 			}
-			if err := os.Chown((targetPath), -1, gid); err != nil {
+			if err := os.Lchown((targetPath), -1, gid); err != nil {
 				return nil, status.Error(codes.Internal, fmt.Sprintf("Failed to set GID of root directory %s to %d failed with %v", targetPath, gid, err))
 			}
 		}
