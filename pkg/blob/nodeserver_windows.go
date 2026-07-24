7c9f3dba22f93a91895c04fbafb6c4949b6a36e7
andyzhangx <xiazhang@microsoft.com>
2026-07-24 06:22:51 +0000

    fix: place build tags before license and update boilerplate checker
    gofmt requires //go:build to be the very first line of the file, before
    any comments (including the license block). The boilerplate checker did
    not strip //go:build lines before comparison, causing a false failure.
    
    Fix both:
    - Move //go:build + // +build to the top of nodeserver_{unix,windows}.go
    - Update boilerplate.py regex to strip //go:build lines (like it already
      does for // +build)

--- pkg/blob/nodeserver_windows.go
+++ pkg/blob/nodeserver_windows.go
@@ -1,3 +1,6 @@
+//go:build windows
+// +build windows
+
 /*
 Copyright 2017 The Kubernetes Authors.
 
@@ -13,8 +16,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
-//go:build windows
 
 package blob
 
