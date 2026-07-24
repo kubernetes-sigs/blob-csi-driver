5f5a70a456bde71c1c14e30d584092ab411c36e8
andyzhangx <xiazhang@microsoft.com>
2026-07-24 03:36:24 +0000

    fix boilerplate: move build tags below license header

--- pkg/blob/nodeserver_windows.go
+++ pkg/blob/nodeserver_windows.go
@@ -1,6 +1,3 @@
-//go:build windows
-// +build windows
-
 /*
 Copyright 2017 The Kubernetes Authors.
 
@@ -16,6 +13,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
+//go:build windows
+// +build windows
 
 package blob
 
